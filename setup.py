41b83e88fcd04c87daa257a9c0d8d83183aa138d
Daniel Kollmann <dankolle@mail.de>
2022-02-06 17:19:51 +0100

    Updated config.

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 from distutils.core import setup
+
 setup(
-	name = 'furiganamaker',         # How you named your package folder (MyLib)
-	packages = ['furiganamaker'],   # Chose the same as "name"
-	version = '1.0',      # Start with a small number and increase it with every change you make
-	license = 'gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-	description = 'This library allows you to add furigana to Japanese kanji.',   # Give a short description about your library
-	author = 'Daniel Kollmann',                   # Type in your name
-	author_email = 'dankolle@mail.de',      # Type in your E-Mail
-	url = 'https://github.com/dkollmann/furiganamaker',   # Provide either the link to your github or to your website
-	download_url = 'https://github.com/dkollmann/furiganamaker/archive/refs/tags/1.0.tar.gz',    # I explain this later on
-	keywords = ['furigana', 'japanese', 'kanji', 'mecab', 'pykakasi', 'kakasi', 'jamdict'],   # Keywords that define your package best
-	package_data={'': ['example_textfile_input.txt']},
-	include_package_data=True,
-	install_requires=[            # I get to this in a second
+	name = 'furiganamaker',
+	packages = ['furiganamaker'],
+	version = '1.0',
+	license = 'gpl-3.0',
+	description = 'This library allows you to add furigana to Japanese kanji.',
+	author = 'Daniel Kollmann',
+	author_email = 'dankolle@mail.de',
+	url = 'https://github.com/dkollmann/furiganamaker',
+	download_url = 'https://github.com/dkollmann/furiganamaker/archive/refs/tags/1.0.tar.gz',
+	keywords = ['furigana', 'japanese', 'kanji', 'mecab', 'pykakasi', 'kakasi', 'jamdict'],
+	package_data = {'': ['example_textfile_input.txt']},
+	include_package_data = True,
+	install_requires = [
 		'mecab-python3',
 		'unidic',
 		'pykakasi',
 	],
-	classifiers=[
-		'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-		'Intended Audience :: Developers',      # Define that your audience are developers
-		'Topic :: Software Development :: Build Tools',
-		'License :: OSI Approved :: MIT License',   # Again, pick a license
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Natural Language :: English',
+		'Natural Language :: Japanese',
 		'Programming Language :: Python :: 3.9',
+		'Topic :: Software Development :: Localization',
+		'Topic :: Text Processing :: Linguistic',
 	],
 )
